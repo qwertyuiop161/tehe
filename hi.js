150233deaeeeda3ed43e321f893df1ef9874631b
qwertyuiop161 <84034410+qwertyuiop161@users.noreply.github.com>
2024-02-23 10:27:05 -0500

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,1 +1,87 @@
-
+handsfree.useGesture({
+  "name": "ok",
+  "algorithm": "fingerpose",
+  "models": "hands",
+  "confidence": 7.5,
+  "description": [
+    [
+      "addCurl",
+      "Thumb",
+      "NoCurl",
+      1
+    ],
+    [
+      "addDirection",
+      "Thumb",
+      "DiagonalUpRight",
+      1
+    ],
+    [
+      "addDirection",
+      "Thumb",
+      "HorizontalRight",
+      0.16666666666666666
+    ],
+    [
+      "addCurl",
+      "Index",
+      "NoCurl",
+      1
+    ],
+    [
+      "addDirection",
+      "Index",
+      "DiagonalUpRight",
+      1
+    ],
+    [
+      "addDirection",
+      "Index",
+      "VerticalUp",
+      0.05
+    ],
+    [
+      "addCurl",
+      "Middle",
+      "NoCurl",
+      1
+    ],
+    [
+      "addDirection",
+      "Middle",
+      "VerticalUp",
+      1
+    ],
+    [
+      "addDirection",
+      "Middle",
+      "DiagonalUpRight",
+      0.4
+    ],
+    [
+      "addCurl",
+      "Ring",
+      "NoCurl",
+      1
+    ],
+    [
+      "addDirection",
+      "Ring",
+      "VerticalUp",
+      1
+    ],
+    [
+      "addCurl",
+      "Pinky",
+      "NoCurl",
+      1
+    ],
+    [
+      "addDirection",
+      "Pinky",
+      "DiagonalUpLeft",
+      1
+    ]
+  ],
+  "enabled": true
+})
